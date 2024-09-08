696a5ec5efa202660d8154773a5c01cb150b35d0
sajib689 <jibonraja11@gmail.com>
2024-09-08 11:37:52 +0600

    add navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
+    const links = 
+    <>
+    <li><Link href='/'>Home</Link></li>
+    <li><Link href='/'>About</Link></li>
+    <li><Link href='/'>Services</Link></li>
+    <li><Link href='/'>Blog</Link></li>
+    <li><Link href='/'>Contact</Link></li>
+    </>
   return (
-    <div className="navbar bg-base-100">
+   <div className=' bg-base-100'>
+     <div className="container mx-auto navbar">
     <div className="navbar-start">
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -20,38 +32,23 @@ const Navbar = () => {
         <ul
           tabIndex={0}
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
+         {links}
         </ul>
       </div>
-      <a className="btn btn-ghost text-xl">daisyUI</a>
+      <Link href='/'>
+      <Image src='/assets/logo.svg' width={100} height={60} alt='logo'/>
+      </Link>
     </div>
     <div className="navbar-center hidden lg:flex">
       <ul className="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul className="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
-        </li>
-        <li><a>Item 3</a></li>
+       {links}
       </ul>
     </div>
     <div className="navbar-end">
       <a className="btn">Button</a>
     </div>
   </div>
+   </div>
   );
 };
 
