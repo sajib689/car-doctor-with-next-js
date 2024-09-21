bf86d7eef9b5ff877d6cbf74a9713f10085bafa6
sajib689 <jibonraja11@gmail.com>
2024-09-21 17:13:41 +0600

    add metadta

--- src/app/services/[id]/page.js
+++ src/app/services/[id]/page.js
@@ -1,13 +1,17 @@
 import { getServicesDetails } from "@/services/getAllServices";
 import Image from "next/image";
 
+export const getMetadata = async ({params}) => {
+    
+}
+
 const Page = async ({ params }) => {
     const services = await getServicesDetails(params?.id);  
-    console.log("Image URL:", services?.result?.img);
+   
 
     return (
         <div>
-           <Image src={services?.result?.img} width={850} height={850} alt={services?.result?.title}/>
+           <Image src={services?.result?.img} width={1150} height={400} alt={services?.result?.title}/>
            <h1>Service Name: {services?.result?.title}</h1>
            <p>{services?.result?.description}</p>
            <p>Price: {services?.result?.price}</p>
