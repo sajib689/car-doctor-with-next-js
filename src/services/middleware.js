d5b28ce1d2315a65b624f00c452d2f796282a3d3
sajib689 <jibonraja11@gmail.com>
2024-09-24 12:22:41 +0600

    add new url in middleware

--- src/services/middleware.js
+++ src/services/middleware.js
@@ -1,9 +1,15 @@
-export const middleware = async () => {
+import { cookies } from "next/headers"
+import { NextResponse } from "next/server"
 
+export const middleware = async (request) => {
+    const token = await cookies(request).get('next-auth.session-token')
+    if(!token) {
+        return NextResponse.redirect(new URL('/login'))
+    }
 }
 
 export const config = {
     matcher: [
-        
+
     ]
 }
