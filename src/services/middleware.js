3cb9dfa72d107d81a817b30099f1b1ec5c8f1d27
sajib689 <jibonraja11@gmail.com>
2024-09-24 12:52:51 +0600

    end

--- src/services/middleware.js
+++ src/services/middleware.js
@@ -1,15 +1,19 @@
-import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
+import { cookies } from "next/headers"
 
 export const middleware = async (request) => {
-    const token = await cookies(request).get('next-auth.session-token')
-    if(!token) {
-        return NextResponse.redirect(new URL('/login'))
+    const cookieStore = cookies()
+    const token = cookieStore.get('next-auth.session-token')
+
+    if (!token) {
+        return NextResponse.redirect(new URL('/login', request.url))
     }
+
+    return NextResponse.next()
 }
 
 export const config = {
     matcher: [
-
-    ]
+        '/booking/:path*',
+    ],
 }
